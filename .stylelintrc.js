6d367bafaa51769a5559efca0c7c08556fa5cbfe
kim <kimseongrim@qq.com>
2020-03-15 16:35:47 +0800

    chore: update stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,10 +1,3 @@
 module.exports = {
-  extends: 'stylelint-config-recommended-scss',
-  rules: {
-  'selector-pseudo-element-no-unknown': [ true, {
-    ignorePseudoElements: ['v-deep']
-  } ],
-    // 禁止低优先级的选择器出现在高优先级的选择器之后
-    'no-descending-specificity': null
-}
+  extends: 'stylelint-config-recommended-scss'
 }
